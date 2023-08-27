8b6c860156826473ff7a4fbde27f5368cdaf2feb
LuPeBreak <lfbmrj15@gmail.com>
2023-08-27 11:46:39 -0300

    Update: - changed controllers routes to use api/ before - ProductController add get, getById, post & put methods - TransactionController add get, getById & post  methods

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,17 +1,80 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MStarSupply.Data;
 using MStarSupply.Models;
 
 namespace MStarSupply.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class ProductController : ControllerBase
 {
-    private readonly ILogger<ProductController> _logger;
+  private readonly ILogger<ProductController> _logger;
+  private readonly ApiDbContext db;
 
-    public ProductController(ILogger<ProductController> logger)
+  public ProductController(ILogger<ProductController> logger, ApiDbContext context)
+  {
+    _logger = logger;
+    db = context;
+  }
+
+
+  [HttpGet()]
+  public async Task<IActionResult> Get()
+  {
+    var allProducts = await db.Products.ToListAsync();
+
+    return Ok(allProducts);
+  }
+
+  [HttpGet]
+  [Route("{id}")]
+  public async Task<IActionResult> GetById(int id)
+  {
+    var product = await db.Products.FindAsync(id);
+
+    if (product == null) return NotFound();
+
+    return Ok(product);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Post([FromBody] Product product)
+  {
+    await db.Products.AddAsync(product);
+    await db.SaveChangesAsync();
+
+    return CreatedAtAction(nameof(GetById),
+        new { id = product.Id },
+        product
+    );
+
+  }
+
+  [HttpPut]
+  public async Task<IActionResult> Put(int id, [FromBody] Product product)
+  {
+    try
     {
-        _logger = logger;
+      if (id != product.Id) return BadRequest("Product ID mismatch");
+
+      var productToUpdate = await db.Products.FindAsync(id);
+
+      if (productToUpdate == null) return NotFound($"Product with Id = {id} not found");
+
+      db.Update(product);
+
+      await db.SaveChangesAsync();
+
+      return NoContent();
     }
+    catch (System.Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error updating product");
+    }
+
+  }
+
 
 }
--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,17 +1,76 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MStarSupply.Data;
 using MStarSupply.Models;
 
 namespace MStarSupply.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class TransactionController : ControllerBase
 {
-    private readonly ILogger<TransactionController> _logger;
+  private readonly ILogger<TransactionController> _logger;
+  private readonly ApiDbContext db;
 
-    public TransactionController(ILogger<TransactionController> logger)
+  public TransactionController(ILogger<TransactionController> logger, ApiDbContext context)
+  {
+    _logger = logger;
+    db = context;
+  }
+
+  [HttpGet()]
+  public async Task<IActionResult> Get()
+  {
+    var allTransactions = await db.Transactions.ToListAsync();
+
+    return Ok(allTransactions);
+  }
+
+  [HttpGet]
+  [Route("{id}")]
+  public async Task<IActionResult> GetById(int id)
+  {
+    var transaction = await db.Transactions.FindAsync(id);
+
+    if (transaction == null) return NotFound();
+
+    return Ok(transaction);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Post([FromBody] Transaction transaction)
+  {
+    // ToDo: Update Product quantity when creating a transaction
+    // por hora essa responsabilidade esta no front
+    // using var databaseTransaction = db.Database.BeginTransaction();
+
+    try
     {
-        _logger = logger;
+      await db.Transactions.AddAsync(transaction);
+      await db.SaveChangesAsync();
+
+      var updatedProduct = await db.Products.FindAsync(transaction.ProductId);
+
+      // if("income" == transaction.Type){
+      //   updatedProduct.Quantity += transaction.Quantity;
+      // }
+      // else if(transaction.Type == "outcome"){
+      //   updatedProduct.Quantity += transaction.Quantity;
+      // } // Deveria usar Patch Delta<product>
+      //await db.SaveChangesAsync()
+      // databaseTransaction.Commit();
+
+      return CreatedAtAction(nameof(GetById),
+          new { id = transaction.Id },
+          transaction
+      );
     }
+    catch (System.Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error creating transaction");
+    }
+
+  }
 
 }
